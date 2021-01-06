cdbb2913adb7b43ceb9b737014c34ad707caefa0
Alejandro Estrada <aestrada611@gmail.com>
2021-01-06 11:48:14 -0800

    Adds component files, added page files, and completed routing.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from './components/Header.js'
+import Footer from './components/Footer.js'
+import Home from './pages/Home.js'
+import CatEdit from './pages/CatEdit.js'
+import CatIndex from './pages/CatIndex.js'
+import CatNew from './pages/CatNew.js'
+import CatShow from './pages/CatShow.js'
+import NotFound from './pages/NotFound.js'
 
+
+
+class App extends Component{
+  render(){
+      return (
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={ Home } />
+            <Route path='/catindex' component={ CatIndex } />
+            <Route path='/catshow' component={ CatShow } />
+            <Route path='/catnew' component={ CatNew } />
+            <Route path='/catedit' component={ CatEdit } />
+            <Route component={ NotFound } />
+          </Switch>
+          <Footer />
+        </Router>
+        );
+        }
+      }
 export default App;
